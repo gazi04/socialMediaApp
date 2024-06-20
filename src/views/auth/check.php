cb90ef4d387f7e161ed4f156ba62a13725f18f32
gazi04 <gazmendhalili2016@gmail.com>
2024-06-20 13:01:27 +0200

    Removing unnecessary code.

--- src/views/auth/check.php
+++ src/views/auth/check.php
@@ -1,7 +1,6 @@
 <?php
     include_once "../../config.php";
     include_once BASE_PATH . "/src/models/User.php";
-    $PATH = BASE_PATH . "src/views/auth/login.php";
 
     if(session_status() == PHP_SESSION_NONE){session_start();}
 
