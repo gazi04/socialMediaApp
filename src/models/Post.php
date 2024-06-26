02eaaa16d8ab0b5c1a2d43f74391c7ead25fa2ff
gazi04 <gazmendhalili2016@gmail.com>
2024-06-26 11:04:21 +0200

    Adding an update function to update the posts that are stored in the database, the user can only change the caption of a post.

--- src/models/Post.php
+++ src/models/Post.php
@@ -9,23 +9,30 @@ class Post{
     }
 
     public function create($userId, $imageData, $caption){
-        $this->db->query('INSERT INTO posts (UserID, Post, Caption) VALUES (:user_id, :post, :caption)');
-        $this->db->bind(':user_id', $userId);
-        $this->db->bind(':post', $imageData, PDO::PARAM_LOB);
-        $this->db->bind(':caption', $caption);
+        $this->db->query("INSERT INTO posts (UserID, Post, Caption) VALUES (:user_id, :post, :caption)");
+        $this->db->bind(":user_id", $userId);
+        $this->db->bind(":post", $imageData, PDO::PARAM_LOB);
+        $this->db->bind(":caption", $caption);
         return $this->db->execute();
     }
 
     public function delete($postId){
-        $this->db->query("DELETE FROM `posts` WHERE `PostID` = ':postId'");
+        $this->db->query("DELETE FROM `posts` WHERE `PostID` = :postId");
         $this->db->bind(":postId", $postId);
         return $this->db->execute();
     }
 
     public function getPostsByUserId($userId){
-        $this->db->query("SELECT `PostID`, `Post`, `Caption` FROM `posts` WHERE `UserID` = ':userId' ORDER BY `CreateAt` DESC;");
+        $this->db->query("SELECT `PostID`, `Post`, `Caption` FROM `posts` WHERE `UserID` = :userId ORDER BY `CreateAt` DESC;");
         $this->db->bind(":userId", $userId);
         return $this->db->resultSet();
+    }
+
+    public function update($userId, $caption){
+        $this->db->query("UPDATE `posts` SET `Caption`=:caption WHERE `PostID` = :userId");
+        $this->db->bind(":caption", $caption);
+        $this->db->bind(":userId", $userId);
+        return $this->db->execute();
     }
 }
 ?>
