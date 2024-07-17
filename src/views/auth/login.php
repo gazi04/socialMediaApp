495614dbe3a7d8949400a7384a96f07c2fb3cabc
gazi04 <gazmendhalili2016@gmail.com>
2024-07-17 20:45:41 +0200

    Adding the template to registration page.

--- src/views/auth/login.php
+++ src/views/auth/login.php
@@ -24,65 +24,60 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html>
-	<head>
-		<title>Login</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="../../assets/css/main.css" />
-		<noscript><link rel="stylesheet" href="../../assets/css/noscript.css" /></noscript>
-	</head>
-	<body class="is-preload">
-    <header id="header">
-      <a href="#" class="title">Hyperspace</a>
-      <nav>
-        <ul>
-          <li><a href="register.php">Create an account</a></li>
-        </ul>
-      </nav>
-    </header>
+<head>
+  <title>Login</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+  <link rel="stylesheet" href="../../assets/css/main.css" />
+  <noscript><link rel="stylesheet" href="../../assets/css/noscript.css" /></noscript>
+</head>
+<body class="is-preload">
+  <header id="header">
+    <a href="#" class="title">Hyperspace</a>
+    <nav>
+      <ul>
+        <li><a href="register.php">Create an account</a></li>
+      </ul>
+    </nav>
+  </header>
 
-    <div id="wrapper">
-				<!-- Main -->
-					<section id="main" class="wrapper">
-						<div class="inner">
-							<h1 class="major">Login Form</h1>
-              <form method="post" action="login.php">
-                <div class="row gtr-uniform">
-                  <div class="col-6 col-12-xsmall">
-                    <input type="text" name="username" placeholder="Username" required/>
-                  </div>
-                  <div class="col-6 col-12-xsmall">
-                    <input type="password" name="password" placeholder="Passord" required/>
-                  </div>
-                  <div class="col-12">
-                    <ul class="actions">
-                      <li><input type="submit" value="Login" class="primary" /></li>
-                    </ul>
-                  </div>
-                </div>
-              </form>
+  <div id="wrapper">
+    <section id="main" class="wrapper">
+      <div class="inner">
+        <h1 class="major">Login Form</h1>
+        <form method="post" action="login.php">
+          <div class="row gtr-uniform">
+            <div class="col-6 col-12-xsmall">
+              <input type="text" name="username" placeholder="Username" required/>
             </div>
-					</section>
+            <div class="col-6 col-12-xsmall">
+              <input type="password" name="password" placeholder="Passord" required/>
+            </div>
+            <div class="col-12">
+              <ul class="actions">
+                <li><input type="submit" value="Login" class="primary" /></li>
+              </ul>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  </div>
 
-			</div>
+  <footer id="footer" class="wrapper alt">
+    <div class="inner">
+      <ul class="menu">
+        <li>&copy; Gazmend Halili. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+      </ul>
+    </div>
+  </footer>
 
-   		<!-- Footer -->
-			<footer id="footer" class="wrapper alt">
-				<div class="inner">
-					<ul class="menu">
-						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-					</ul>
-				</div>
-			</footer>
-
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-
-	</body>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.scrollex.min.js"></script>
+  <script src="assets/js/jquery.scrolly.min.js"></script>
+  <script src="assets/js/browser.min.js"></script>
+  <script src="assets/js/breakpoints.min.js"></script>
+  <script src="assets/js/util.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
 </html>
--- src/views/auth/register.php
+++ src/views/auth/register.php
@@ -17,11 +17,70 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 }
 ?>
 
-<form action="register.php" method="POST">
-    <input type="text" name="username" placeholder="Username" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <input type="email" name="email" placeholder="Email" required>
-    <button type="submit">Register</button>
-</form>
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+  <title>Login</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+  <link rel="stylesheet" href="../../assets/css/main.css" />
+  <noscript><link rel="stylesheet" href="../../assets/css/noscript.css" /></noscript>
+</head>
+<body class="is-preload">
+  <header id="header">
+    <a href="#" class="title">Hyperspace</a>
+    <nav>
+      <ul>
+        <li><a href="login.php">Log In</a></li>
+      </ul>
+    </nav>
+  </header>
 
-<a href="login.php">Log In</a>
+  <div id="wrapper">
+    <section id="main" class="wrapper">
+      <div class="inner">
+        <h1 class="major">Create an account</h1>
+        <form method="post" action="register.php">
+          <div class="row gtr-uniform">
+            <div class="col-6 col-12-xsmall">
+              <input type="text" name="username" placeholder="Username" required/>
+            </div>
+            <div class="col-6 col-12-xsmall">
+              <input type="password" name="password" placeholder="Passord" required/>
+            </div>
+            <div class="col-6 col-12-xsmall">
+              <input type="email" name="email" placeholder="Email" required>
+            </div>
+            <div class="col-12">
+              <ul class="actions">
+                <li><input type="submit" value="Register" class="primary" /></li>
+              </ul>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  </div>
+
+  <footer id="footer" class="wrapper alt">
+    <div class="inner">
+      <ul class="menu">
+        <li>&copy; Gazmend Halili. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+      </ul>
+    </div>
+  </footer>
+
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.scrollex.min.js"></script>
+  <script src="assets/js/jquery.scrolly.min.js"></script>
+  <script src="assets/js/browser.min.js"></script>
+  <script src="assets/js/breakpoints.min.js"></script>
+  <script src="assets/js/util.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
