ec548311a8e9c072b546e4ee067c109ea99d1bc9
gazi04 <gazmendhalili2016@gmail.com>
2024-07-19 20:28:26 +0200

    Breaking the repeated components in seperate folders.

---
+++ src/components/footer.php
@@ -0,0 +1,7 @@
+  <footer id="footer" class="wrapper alt">
+    <div class="inner">
+      <ul class="menu">
+        <li>&copy; Gazmend Halili. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+      </ul>
+    </div>
+  </footer>
---
+++ src/components/navbar.php
@@ -0,0 +1,8 @@
+<nav>
+  <ul>
+    <li><a href="../feed/index.php">Feed</a></li>
+    <li><a href="../profile/index.php">Profile</a></li>
+    <li><a href="../users/search.php">Search</a></li>
+    <li><a href="../auth/logout.php">Logout</a></li>
+  </ul>
+</nav>
