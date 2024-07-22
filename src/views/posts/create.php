9c71aa89630298e6a7c4bb5ee2012858533641ba
gazi04 <gazmendhalili2016@gmail.com>
2024-07-22 07:51:42 +0200

    Adding the template to the page.

--- src/views/posts/create.php
+++ src/views/posts/create.php
@@ -16,18 +16,41 @@
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Post</title>
+  <title>Create Post</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+  <link rel="stylesheet" href="../../assets/css/main.css" />
+  <noscript><link rel="stylesheet" href="../../assets/css/noscript.css" /></noscript>
 </head>
-<body>
-    <form method="post" action="create.php" enctype="multipart/form-data">
-        <input type="file" name="image" required>
-        <textarea name="caption" placeholder="Add a caption..." required></textarea>
-        <button type="submit">Post Image</button>
-    </form>
+<body class="is-preload">
+
+  <header id="header">
+    <a href="index.html" class="title">Profile</a>
+    <?php include(BASE_PATH."/src/components/navbar.php"); ?>
+  </header>
+
+  <div id="wrapper">
+    <section id="main" class="wrapper">
+      <div class="inner">
+        <h1 class="major">Create a Post</h1>
+        <form method="post" action="create.php" enctype="multipart/form-data">
+            <input type="file" name="image" required style="margin-bottom: 3%;">
+            <textarea name="caption" placeholder="Add a caption..." required style="margin-bottom:3%"></textarea>
+            <button type="submit">Post Image</button>
+        </form>
+      </div>
+    </section>
+  </div>
+
+  <?php include(BASE_PATH."/src/components/footer.php"); ?>
+  <?php include(BASE_PATH."/src/components/scripts.php"); ?>
 </body>
 </html>
