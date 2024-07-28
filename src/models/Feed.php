3989fce4fe2c54e57677778a5b4ace543eea4299
gazi04 <gazmendhalili2016@gmail.com>
2024-07-28 15:33:58 +0200

    Adding function for getting feeds from followers, fromm non followers and with the most likes.

--- src/models/Feed.php
+++ src/models/Feed.php
@@ -18,5 +18,46 @@ class Feed{
     $this->db->bind(":userId", $userId);
     return $this->db->execute();
   }
+  
+  public function getFeedFromFollowers($userId) {
+    $this->db->query("SELECT posts.*, users.Username 
+      FROM posts 
+      JOIN users ON posts.UserID = users.UserID 
+      JOIN followers ON posts.UserID = followers.FollowingUserID 
+      WHERE followers.FollowerUserID = :userId
+      ORDER BY posts.CreateAt DESC;
+    ");
+    $this->db->bind(":userId", $userId);
+    return $this->db->resultSet();
+  }
+
+  public function getFeedFromNonFollowers($userId) {
+    $this->db->query("SELECT posts.*, users.Username 
+      FROM posts 
+      JOIN users ON posts.UserID = users.UserID 
+      WHERE posts.UserID NOT IN (
+          SELECT FollowingUserID 
+          FROM followers 
+          WHERE FollowerUserID = :userId
+      ) AND posts.UserID != :userId 
+      ORDER BY posts.CreateAt DESC;
+    ");
+    $this->db->bind(":userId", $userId);
+    return $this->db->resultSet();
+    
+  }
+
+  public function getFeedWithMostLike($userId) {
+    $this->db->query("
+      SELECT posts.*, users.Username, COUNT(postlikes.PostID) as like_count 
+      FROM posts 
+      JOIN users ON posts.UserID = users.UserID 
+      LEFT JOIN postlikes ON posts.PostID = postlikes.PostID 
+      GROUP BY posts.PostID 
+      ORDER BY like_count DESC, posts.CreateAt DESC;
+    ");
+    $this->db->bind(":userId", $userId);
+    return $this->db->resultSet();
+  }
 }
 ?>
