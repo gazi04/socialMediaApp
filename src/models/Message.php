cfccb9faab6d869c1f2ac7f3439c5a93e3f52b37
gazi04 <gazmendhalili2016@gmail.com>
2024-07-31 22:19:02 +0200

    Adding the message model.

---
+++ src/models/Message.php
@@ -0,0 +1,27 @@
+<?php 
+require_once "../../Database.php";
+
+class Message{
+  private $db;
+
+  public function __construct(){
+    $this->db = new Database();
+  }
+
+  public function saveMessage($sender, $receiver, $msg){
+    $this->db->query("INSERT INTO `messages`(`SenderID`, `ReceiverID`, `Message`) VALUES (':sender',':receiver',':message')");
+    $this->db->bind(":sender", $sender);
+    $this->db->bind(":receiver", $receiver);
+    $this->db->bind(":message", $msg);
+    return $this->db->execute();
+  }
+
+  public function getChatHistory($firstUser, $secondUser){
+    $this->db->query("SELECT `SenderID`, `ReceiverID`, `Message`, `CreateAt` FROM `messages` WHERE `SenderID` == :firstUser AND `ReceiverID` == :secondUser; ORDER BY `CreateAt` ASC");
+    $this->db->bind(":firstUser", $firstUser);
+    $this->db->bind(":secondUser", $secondUser);
+    return $this->db->execute();
+
+  }
+}
+?>
