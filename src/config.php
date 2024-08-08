b021be80e9981881dc09e3b84c560eea6d47c7ad
gazi04 <gazmendhalili2016@gmail.com>
2024-08-08 11:10:27 +0200

    Some problems with neovim and git so need to push all the files.

--- src/config.php
+++ src/config.php
@@ -2,7 +2,7 @@
 define('BASE_PATH', dirname(__DIR__));
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'root2024');
 define('DB_NAME', 'instaclone');
 ?>
 
