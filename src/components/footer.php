060466c51dd4114608ffa97683966f540423d4e0
gazi04 <gazmendhalili2016@gmail.com>
2024-09-21 17:03:19 +0200

    Implementing the new template in the navbar and footer component.

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,8 +1,8 @@
-  <footer id="footer" class="wrapper alt">
-    <div class="inner">
-      <ul class="menu" style="color: #ffffff;">
-        <li>&copy; Gazmend Halili. All rights reserved.</li>
-        <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-      </ul>
-    </div>
-  </footer>
+<footer id="footer" class="wrapper alt">
+  <div class="">
+    <ul class="">
+      <li>&copy; Gazmend Halili. All rights reserved.</li>
+      <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+    </ul>
+  </div>
+</footer> 
--- src/components/navbar.php
+++ src/components/navbar.php
@@ -1,8 +1,64 @@
 <nav>
-  <ul>
-    <li><a href="../feed/index.php">Feed</a></li>
-    <li><a href="../profile/index.php">Profile</a></li>
-    <li><a href="../users/search.php">Search</a></li>
-    <li><a href="../auth/logout.php">Logout</a></li>
+  <ul class="menu">
+    <div id="banner">
+      <a href="#">Welcome, test</a>
+    </div>
+
+    <li>
+      <a href="#" onmouseover="highlightIcon(this, '../assets/icons/blackHome.png')" onmouseout="unHighlightIcon(this, '../assets/icons/whiteHome.png')">
+        <div class="icon"><img src="../assets/icons/whiteHome.png" /></div>
+        <div class="page"><span><a href="../feed/index.php">Feed</a></span></div>
+      </a>
+    </li>
+    <li>
+      <a href="#" onmouseover="highlightIcon(this, '../assets/icons/highlightedSearch.png')" onmouseout="unHighlightIcon(this, '../assets/icons/search.png')">
+        <div class="icon"><img src="../assets/icons/search.png" /></div>
+        <div class="page"><span><a href="../users/search.php">Search</a></span></div>
+      </a>
+    </li>
+    <li>
+      <a href="#" onmouseover="highlightIcon(this, '../assets/icons/highlightedCompass.png')" onmouseout="unHighlightIcon(this, '../assets/icons/compass.png')">
+        <div class="icon"><img src="../assets/icons/compass.png" /></div>
+        <div class="page"><span><a href="#">Discover</a></span></div>
+      </a>
+    </li>
+    <li>
+      <a href="#" onmouseover="highlightIcon(this, '../assets/icons/highlightedClapperboard.png')" onmouseout="unHighlightIcon(this, '../assets/icons/clapperboard.png')">
+        <div class="icon"><img src="../assets/icons/clapperboard.png" /></div>
+        <div class="page"><span><a href="#">Reels</a></span></div>
+      </a>
+    </li>
+    <li>
+      <a href="#" onmouseover="highlightIcon(this, '../assets/icons/sendBlack.png')" onmouseout="unHighlightIcon(this, '../assets/icons/send.png')">
+        <div class="icon"><img src="../assets/icons/send.png" /></div>
+        <div class="page"><span><a href="../chat/index.php">Messages</a></span></div>
+      </a>
+    </li>
+    <li>
+      <a href="#" onmouseover="highlightIcon(this, '../assets/icons/highlightedHeart.png')" onmouseout="unHighlightIcon(this, '../assets/icons/heart.png')">
+        <div class="icon"><img src="../assets/icons/heart.png" /></div>
+        <div class="page"><span><a href="#">Notifications</a></span></div>
+      </a>
+    </li>
+    <li>
+      <a href="#" onmouseover="highlightIcon(this, '../assets/icons/highlightedCreate.png')" onmouseout="unHighlightIcon(this, '../assets/icons/create.png')">
+        <div class="icon"><img src="../assets/icons/create.png" /></div>
+        <div class="page"><span><a href="#">Create</a></span></div>
+      </a>
+    </li>
+    <li>
+      <a href="#" onmouseover="highlightIcon(this, '../assets/icons/blackHome.png')" onmouseout="unHighlightIcon(this, '../assets/icons/whiteHome.png')">
+        <div class="icon"><img src="../assets/icons/home.png" /></div>
+        <div class="page"><span><a href="#">Profile</a></span></div>
+      </a>
+    </li>
+    <li class="last-item">
+      <a href="#" onmouseover="highlightIcon(this, '../assets/icons/blackHome.png')" onmouseout="unHighlightIcon(this, '../assets/icons/whiteHome.png')">
+        <div class="icon"><img src="../assets/icons/home.png" /></div>
+        <div class="page"><span><a href="../auth/logout.php">Logout</a></span></div>
+      </a>
+    </li>
   </ul>
+
+  <div class="vertical-line"></div>
 </nav>
