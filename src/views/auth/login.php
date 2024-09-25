80376441cbe91c8e4a845e514f949bdaf3b2e3ad
gazi04 <gazmendhalili2016@gmail.com>
2024-09-25 19:50:02 +0200

    Implementing the new template.

--- src/views/auth/login.php
+++ src/views/auth/login.php
@@ -17,54 +17,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
+
 <!DOCTYPE HTML>
-<!--
-  Hyperspace by HTML5 UP
-  html5up.net | @ajlkn
-  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
-<head>
-  <title>Login</title>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-  <link rel="stylesheet" href="../../assets/css/main.css" />
-  <noscript><link rel="stylesheet" href="../../assets/css/noscript.css" /></noscript>
-</head>
-<body class="is-preload">
-  <header id="header">
-    <a href="#" class="title">Hyperspace</a>
-    <nav>
-      <ul>
-        <li><a href="register.php">Create an account</a></li>
-      </ul>
-    </nav>
-  </header>
-
-  <div id="wrapper">
-    <section id="main" class="wrapper">
-      <div class="inner">
-        <h1 class="major">Login Form</h1>
-        <form method="post" action="login.php">
-          <div class="row gtr-uniform">
-            <div class="col-6 col-12-xsmall">
-              <input type="text" name="username" placeholder="Username" required/>
-            </div>
-            <div class="col-6 col-12-xsmall">
-              <input type="password" name="password" placeholder="Passord" required/>
-            </div>
-            <div class="col-12">
-              <ul class="actions">
-                <li><input type="submit" value="Login" class="primary" /></li>
-              </ul>
-            </div>
-          </div>
-        </form>
+  <head>
+    <title>Feed</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="../../assets/css/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+  </head>
+  <body style="height: 100%;">
+    <div class="login-container">
+      <div class="login-form">
+        <div class="login-banner"><h1>Tungjatjeta</h1></div>
+        <div class="login-credentials">
+          <form method="post" action="login.php">
+            <input type="text" name="username" placeholder="Username" required/><br><br>
+            <input type="password" name="password" placeholder="Passord" required/><br><br>
+            <input type="submit" value="Login" class="submit_button" />
+          </form>
+        </div>
+        <div class="login-with"></div>
       </div>
-    </section>
-  </div>
+    </div>
 
-  <?php include(BASE_PATH."/src/components/footer.php"); ?>
-  <?php include(BASE_PATH."/src/components/scripts.php"); ?>
-</body>
+    <script type="text/javascript" src="../assets/js/script.js"></script>
+  </body>
 </html>
