c7246a7b650775e4b92b53abe4d6cbad9e1b9b27
gazi04 <gazmendhalili2016@gmail.com>
2024-09-27 22:44:10 +0200

    Adding an icon for liked posts.

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,7 +1,38 @@
+console.log('script.js loaded');
 function highlightIcon(element, boldIcon) {
   element.querySelector("img").src = boldIcon;
 }
 
 function unHighlightIcon(element, originalIcon) {
   element.querySelector("img").src = originalIcon;
+}
+
+function dislikePost(postId, userId) {
+  const request = new XMLHttpRequest();
+  request.open("POST", "../../views/feed/index.php", true);
+
+  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+      console.log(request.responseText);
+    }
+  }
+
+  request.send("likePost=true&postId=" + encodeURIComponent(postId) + "&userId=" + encodeURIComponent(userId))
+}
+
+function likePost(postId, userId) {
+  const request = new XMLHttpRequest();
+  request.open("POST", "../../views/feed/index.php", true);
+
+  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+      console.log(request.responseText);
+    }
+  }
+
+  request.send("dislikePost=true&postId=" + encodeURIComponent(postId) + "&userId=" + encodeURIComponent(userId))
 }
