cc0793294ac4840306c111ac99131fe937868681
gazi04 <gazmendhalili2016@gmail.com>
2024-09-30 12:07:40 +0200

    Removing unneccesary files and moving the config.php and Database.php into the core/ directory.

--- src/core/Database.php
+++ src/core/Database.php
@@ -1,4 +1,7 @@
 <?php
+namespace Core;
+
+require "config.php";
 class Database {
   private $host = DB_HOST;
   private $user = DB_USER;
@@ -10,13 +13,13 @@ class Database {
   public function __construct() {
     $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
     $options = array(
-      PDO::ATTR_PERSISTENT => true,
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+      \PDO::ATTR_PERSISTENT => true,
+      \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
     );
 
     try {
-      $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
-    } catch (PDOException $e) {
+      $this->dbh = new \PDO($dsn, $this->user, $this->pass, $options);
+    } catch (\PDOException $e) {
       echo $e->getMessage();
     }
   }
@@ -29,19 +32,19 @@ class Database {
     if (is_null($type)) {
       switch (true) {
       case is_int($value):
-        $type = PDO::PARAM_INT;
+        $type = \PDO::PARAM_INT;
         break;
       case is_bool($value):
-        $type = PDO::PARAM_BOOL;
+        $type = \PDO::PARAM_BOOL;
         break;
       case is_null($value):
-        $type = PDO::PARAM_NULL;
+        $type = \PDO::PARAM_NULL;
         break;
       case is_resource($value):
-        $type = PDO::PARAM_LOB;
+        $type = \PDO::PARAM_LOB;
         break;
       default:
-        $type = PDO::PARAM_STR;
+        $type = \PDO::PARAM_STR;
       }
     }
     $this->stmt->bindValue($param, $value, $type);
@@ -53,12 +56,12 @@ class Database {
 
   public function resultSet() {
     $this->execute();
-    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $this->stmt->fetchAll(\PDO::FETCH_ASSOC);
   }
 
   public function single() {
     $this->execute();
-    return $this->stmt->fetch(PDO::FETCH_ASSOC);
+    return $this->stmt->fetch(\PDO::FETCH_ASSOC);
   }
 
   public function rowCount() {
---
+++ src/core/config.php
@@ -0,0 +1,20 @@
+<?php
+namespace Core;
+
+if(!defined('BASE_PATH')) {
+  define('BASE_PATH', dirname(__DIR__));
+}
+if(!defined('DB_HOST')) {
+  define('DB_HOST', 'localhost');
+}
+if(!defined('DB_USER')) {
+  define('DB_USER', 'root');
+}
+if(!defined('DB_PASS')) {
+  define('DB_PASS', 'root2024');
+}
+if(!defined('DB_NAME')) {
+  define('DB_NAME', 'instaclone');
+}
+?>
+
