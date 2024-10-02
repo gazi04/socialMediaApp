c876263115d91c9ccac6fc1fd10f38d4bb6dc97e
gazi04 <gazmendhalili2016@gmail.com>
2024-10-02 19:17:50 +0200

    Removing the old template and implementing the new one.

--- src/views/profile/index.php
+++ src/views/profile/index.php
@@ -1,11 +1,11 @@
 <?php
-require_once "../../config.php";
-include_once BASE_PATH . "/src/controllers/UserController.php";
-include_once BASE_PATH . "/src/controllers/PostController.php";
-include_once BASE_PATH . "/src/controllers/FollowController.php";
-include_once BASE_PATH . "/src/controllers/LikeController.php";
-include_once BASE_PATH . "/src/controllers/CommentController.php";
 require_once "../auth/check.php";
+
+use Controllers\UserController;
+use Controllers\PostController;
+use Controllers\FollowController;
+use Controllers\LikeController;
+use Controllers\CommentController;
 
 $userController = new UserController();
 $postController = new PostController();
@@ -21,97 +21,23 @@ $numberOfFollowing =  $followController->getFollowingCount($_SESSION["userId"]);
 
 ?>
 
-
 <!DOCTYPE HTML>
-<!--
-  Hyperspace by HTML5 UP
-  html5up.net | @ajlkn
-  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
-<head>
-  <title>Profile</title>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-  <link rel="stylesheet" href="../../assets/css/main.css" />
-  <noscript><link rel="stylesheet" href="../../assets/css/noscript.css" /></noscript>
-  <style>
-    .comment-box{
-        width: 90%;
-        padding: 10px;
-        margin-bottom: 10px;
-    }
-    .comment-box p{
-        margin: 5px 0;
-    }
-    .comment-box input{
-        width: 100%;
-        border: none;
-        font-size: 17px;
-    }
-    .comment-section{
-      max-height: 145px;
-      overflow-y: auto;
-      padding: 10px;
-    }
-  </style>
-</head>
-<body class="is-preload">
-  <header id="header">
-    <a href="#" class="title">Profile</a>
-    <?php include(BASE_PATH."/src/components/navbar.php"); ?>
-  </header>
+  <head>
+    <title>Your Profile</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="../../assets/css/style.css" />
+  </head>
+  <body>
+    <div class="container-profile">
 
-  <div id="wrapper">
-    <div class="row">
-      <div class="col-4 col-12-medium">
-        <div class="image fit">
-          <img width="30%" src="data:image/jpeg;base64,<?php echo base64_encode($userProfileData["ProfileImage"]); ?>" alt="Post Image" style="margin-top: 9%; margin-left: 2%;">
-        </div>
-      </div>
-      <div class="col-6 col-12-medium" style="padding-top: 5%; padding-left: 3rem;">
-        <div class="row"> 
-           <div class="col-6">
-            <h3>Username: <?php echo $userProfileData["Username"] ?></h3>
-            <h3>Bio: <?php echo $userProfileData["Bio"] ?></h3>
-          </div>
-          <div class="col-6">
-            <ul class="actions">
-              <h3><a href="followers.php">Followers:  <?php echo $numberOfFollowers; ?></a> | <a href="following.php">Following:  <?php echo $numberOfFollowing; ?></a></h3>
-          </ul> 
-        </div>
-        </div>
-        <div class="row" style="padding-top: 5%; padding-left: 5%;">
-          <ul class="actions">
-            <a class="button" href="profileManagement.php">Edit Profile</a>
-            <a  class="button" href="../posts/create.php" style="margin-left: 5%;">Create Post</a>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="row table-wrapper" style="padding-left: 2%;">
-    <?php if (!empty($posts)): ?>
-      <table class="alt" style="padding-left: 2%; padding-right: 2%; padding-top: 2%;">
-        <thead>
-          <tr>
-            <th>Image</th>
-            <th>Caption</th>
-            <th>Likes</th>
-            <th>Comments</th>
-          </tr>
-        </thead>
-        <tbody>
-            <?php include BASE_PATH."/src/components/post.php"; ?>
-        </tbody>
-      </table>
-    <?php else: ?>
-      <p>No posts available.</p>
-    <?php endif; ?>
+      <div class="navbar"><?php include(BASE_PATH."/components/navbar.php"); ?></div>
+
+      <div class="content"><?php include(BASE_PATH."/components/userProfile.php"); ?></div>
     </div>
 
-  </div>
-
-  <?php include(BASE_PATH."/src/components/footer.php"); ?>
-  <?php include(BASE_PATH."/src/components/scripts.php"); ?>
-</body>
+    <script type="text/javascript" src="../../assets/js/script.js"></script>
+  </body>
 </html>
+
