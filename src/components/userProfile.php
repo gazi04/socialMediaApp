c3db539e0f54eec901167d286c23c1460342f744
gazi04 <gazmendhalili2016@gmail.com>
2024-10-02 19:16:32 +0200

    Finishing only the template for the user profile component.'

---
+++ src/components/userProfile.php
@@ -0,0 +1,45 @@
+<div class="user-profile">
+  <div class="profile">
+    <div class="profile-image"><img src="../../assets/images/sunflower.jpg"/></div>
+    <div class="profile-details">
+      <div class="username">
+        <div class="name">Gazmend</div>
+        <div class="options">
+          <a class="follow" style="margin-right: 5px;">Follow</a>
+          <a class="follow">Follow</a>
+        </div>
+      </div>
+      <div class="stats"> 
+        <div id="number-of-posts">Posts: 4</div>&nbsp;
+        <div id="number-of-followers">Followers: 4</div>&nbsp;
+        <div id="number-of-followings">Following: 4</div>
+      </div>
+      <div class="bio">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+          test,
+          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div id="posts">
+    <div class="row">
+      <div class="post"><img src="../../assets/images/sunflower.jpg"/></div>
+      <div class="post"><img src="../../assets/images/sunflower.jpg"/></div>
+      <div class="post"><img src="../../assets/images/sunflower.jpg"/></div>
+    </div>
+    <div class="row">
+      <div class="post"><img src="../../assets/images/sunflower.jpg"/></div>
+      <div class="post"><img src="../../assets/images/sunflower.jpg"/></div>
+      <div class="post"><img src="../../assets/images/sunflower.jpg"/></div>
+    </div>
+    <div class="row">
+      <div class="post"><img src="../../assets/images/sunflower.jpg"/></div>
+      <div class="post"><img src="../../assets/images/sunflower.jpg"/></div>
+      <div class="post"><img src="../../assets/images/sunflower.jpg"/></div>
+    </div>
+  </div>
+</div>
