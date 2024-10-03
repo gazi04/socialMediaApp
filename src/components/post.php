b1c0d88cbd430e196c485f2359de554f16054278
gazi04 <gazmendhalili2016@gmail.com>
2024-10-03 11:49:33 +0200

    Removing unnecessary html code that is used for the old template.

--- src/components/post.php
+++ src/components/post.php
@@ -32,51 +32,6 @@
 /* $postId = $post["PostID"]; */
 /* $postComments = $comments[$postId] ?? []; */
 ?>
-<!-- <tr> -->
-<!--   <input type="hidden" name="userId" value="<?php //echo $profileUserId; ?>"/> -->
-<!--   <td><img src="data:image/jpeg;base64,<?php //echo base64_encode($post["Post"]); ?>" alt="Post Image" style="max-width: 100px;"></td> -->
-<!--   <td><?php //echo htmlspecialchars($post["Caption"]); ?></td> -->
-<!---->
-<!--   <td> -->
-<!--     <form method="POST" action="<?php //echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"> -->
-<!--       <input type="hidden" name="postId" value="<?php //echo $post["PostID"]; ?>"/> -->
-<!--       <input type="hidden" name="userId" value="<?php //echo $profileUserId; ?>"/> -->
-<!--       <?php //echo $likeController->getLikeCount($post["PostID"]); ?> -->
-<!---->
-<!--       <?php //if ($likeController->isLiked($_SESSION["userId"], $post["PostID"])): ?> -->
-<!--           <input type="submit" name="unlike" value="Unlike" style="margin-left: 10%;"> -->
-<!--       <?php //else: ?> -->
-<!--           <input type="submit" name="like" value="Like" style="margin-left: 10%;"> -->
-<!--       <?php //endif; ?> -->
-<!--     </form>  -->
-<!--   </td> -->
-<!---->
-<!--   <td colspan="5"> -->
-<!--   <div class="comment-section"> -->
-<!--      <?php //if (!empty($postComments)): ?> -->
-<!--         <?php //foreach ($postComments as $comment): ?> -->
-<!--           <div class="comment-box"> -->
-<!--               <input type="text" value="<?php //echo htmlspecialchars($comment["Comment"]); ?>" readonly/> -->
-<!--               <p><strong><?php //echo htmlspecialchars($comment["Username"]); ?>:</strong> <?php //echo htmlspecialchars($comment["CreateAt"]); ?></p> -->
-<!--               <hr> -->
-<!--           </div> -->
-<!--         <?php //endforeach; ?> -->
-<!--     <?php //else: ?> -->
-<!--         <p>No comments available.</p> -->
-<!--     <?php //endif; ?> -->
-<!--   </div> -->
-<!--   </td> -->
-<!---->
-<!--   <td> -->
-<!--     <form method="POST" action="<?php //echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"> -->
-<!--       <input type="hidden" name="userId" value="<?php //echo $profileUserId; ?>"/> -->
-<!--       <input type="hidden" name="postId" value="<?php //echo $post["PostID"]; ?>"/> -->
-<!--       <input type="text" name="comment"/>  -->
-<!--       <input type="submit" name="commentPost" value="Comment" style="margin-top: 2%;"/> -->
-<!--     </form> -->
-<!--   </td> -->
-<!-- </tr> -->
-<?php //endforeach; ?>
 
 <?php 
 global $likeController;
@@ -111,7 +66,7 @@ if (isset($_POST["dislikePost"]) && isset($_POST["postId"]) && isset($_POST["use
       </a>
       <a><img src="../../assets/icons/share.png" /></a>
       <a><img src="../../assets/icons/send.png" /></a>
-      <div class="like-counts">Likes: <span id="likes<?php echo $post["PostID"]; ?>"><?php echo $post["LikeCount"] ?></span></div>
+      <div class="like-counts">Likes: <span id="likes"><?php echo $post["LikeCount"] ?></span></div>
     </div>
     <div class="text"> 
       <b><?php echo htmlspecialchars($post["Username"]); ?>: </b> <?php echo htmlspecialchars($post["Caption"]) ?>
