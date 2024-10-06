84cbd60f5d06903d0870b222dc67c7a5c97aa591
gazi04 <gazmendhalili2016@gmail.com>
2024-10-06 22:07:16 +0200

    Using jquery for changing the icons in the navbar.

--- src/assets/js/jquery.js
+++ src/assets/js/jquery.js
@@ -1,22 +1,38 @@
 $(document).ready(function() {
-  // Function to open modal and populate with post data
-  function openModal() {
+  $(".menuOption").hover(
+    function(){
+      const icon = $(this).children(".icon").children("img");
+      $(icon).prop("src", icon.data("highlighted-icon"));
+    },
+    function(){
+      const icon = $(this).children(".icon").children("img");
+      $(icon).prop("src", icon.data("original-icon"));
+    }
+  );
+
+  function openModal(postElement) {
     // Get data attributes from the clicked post
-    // const postId = $(postElement).data('post-id');
-    // const caption = $(postElement).data('caption');
-    // const likes = $(postElement).data('likes');
-    // const username = $(postElement).data('username');
-    // const imageSrc = $(postElement).data('image');
+    const postId = $(postElement).data('post-id');
+    const caption = $(postElement).data('caption');
+    const likes = $(postElement).data('likes');
+    const username = $(postElement).data('username');
+    const imageSrc = $(postElement).data('image');
+    const profilePictureSrc = $(postElement).data('profile-picture')
+
+    $("#modalProfileImage").empty();
+    $(".profile-image").children("img").clone().appendTo("#modalProfileImage");
 
     // Populate the modal with post data
-    // $('#modalImage').attr('src', imageSrc);
-    // $('#modalCaption').text(caption);
-    // $('#modalLikes').text(`Likes: ${likes}`);
-    // $('#modalUsername').text(username);
+    $('#modalImage').prop('src', imageSrc);
+    $('#modalCaption').text(caption);
+    $('#modalLikes').text(`Likes: ${likes}`);
+    $('#modalUsername').text(username);
+
+
+    // $('#modalUserProfile').html(`<img src="data:image/jped;base64, ${profilePictureSrc}"/> <b>${username}</b> ${caption}`);
 
     // Show the modal
     $('#postModal')[0].showModal();
-    console.log($('#postModal')[0])
 
     // Optional: you can store postId if needed for future actions (e.g., liking the post)
     // $('#likeButton').data('post-id', postId);
@@ -31,7 +47,6 @@ $(document).ready(function() {
   $(window).on('click', function(e) {
     if ($(e.target).is('#postModal')) {
       $('#postModal')[0].close();
-      console.log($('#postModal')[0])
     }
   });
 
--- src/components/navbar.php
+++ src/components/navbar.php
@@ -1,5 +1,4 @@
 <?php
-/* include "../core/config.php"; */
 $feedPath = "../feed/index.php";
 $myProfilePath = "../profile/index.php";
 $logoutPath = "../auth/logout.php";
@@ -11,56 +10,50 @@ $logoutPath = "../auth/logout.php";
     </div>
 
     <li>
-      <a href="<?php echo $feedPath ?>" onmouseover="highlightIcon(this, '../../assets/icons/blackHome.png')" onmouseout="unHighlightIcon(this, '../../assets/icons/whiteHome.png')">
-        <div class="icon"><img src="../../assets/icons/whiteHome.png" /></div>
+      <a href="<?php echo $feedPath ?>" class="menuOption">
+        <div class="icon"><img id="menuIcon" src="../../assets/icons/whiteHome.png" data-original-icon="../../assets/icons/whiteHome.png" data-highlighted-icon="../../assets/icons/blackHome.png"/></div>
         <div class="page"><span>Feed</span></div>
       </a>
     </li>
     <li>
-      <a href="#" onmouseover="highlightIcon(this, '../../assets/icons/highlightedSearch.png')" onmouseout="unHighlightIcon(this, '../../assets/icons/search.png')">
-        <div class="icon"><img src="../../assets/icons/search.png" /></div>
+      <a href="#" class="menuOption">
+        <div class="icon"><img id="menuIcon" src="../../assets/icons/search.png" data-original-icon="../../assets/icons/search.png" data-highlighted-icon="../../assets/icons/highlightedSearch.png" /></div>
         <div class="page"><span>Search</span></div>
       </a>
     </li>
     <li>
-      <a href="#" onmouseover="highlightIcon(this, '../../assets/icons/highlightedCompass.png')" onmouseout="unHighlightIcon(this, '../../assets/icons/compass.png')">
-        <div class="icon"><img src="../../assets/icons/compass.png" /></div>
-        <div class="page"><span>Discover</span></div>
-      </a>
-    </li>
-    <li>
-      <a href="#" onmouseover="highlightIcon(this, '../../assets/icons/highlightedClapperboard.png')" onmouseout="unHighlightIcon(this, '../../assets/icons/clapperboard.png')">
-        <div class="icon"><img src="../../assets/icons/clapperboard.png" /></div>
+      <a href="#" class="menuOption">
+        <div class="icon"><img id="menuIcon" src="../../assets/icons/clapperboard.png" data-original-icon="../../assets/icons/clapperboard.png" data-highlighted-icon="../../assets/icons/highlightedClapperboard.png" /></div>
         <div class="page"><span>Reels</span></div>
       </a>
     </li>
     <li>
-      <a href="#" onmouseover="highlightIcon(this, '../../assets/icons/sendBlack.png')" onmouseout="unHighlightIcon(this, '../../assets/icons/send.png')">
-        <div class="icon"><img src="../../assets/icons/send.png" /></div>
+      <a href="#" class="menuOption">
+        <div class="icon"><img id="menuIcon" src="../../assets/icons/send.png" data-original-icon="../../assets/icons/send.png" data-highlighted-icon="../../assets/icons/sendBlack.png"/></div>
         <div class="page"><span>Messages</span></div>
       </a>
     </li>
     <li>
-      <a href="#" onmouseover="highlightIcon(this, '../../assets/icons/highlightedHeart.png')" onmouseout="unHighlightIcon(this, '../../assets/icons/heart.png')">
-        <div class="icon"><img src="../../assets/icons/heart.png" /></div>
+      <a href="#" class="menuOption">
+        <div class="icon"><img id="menuIcon" src="../../assets/icons/heart.png" data-original-icon="../../assets/icons/heart.png" data-highlighted-icon="../../assets/icons/highlightedHeart.png" /></div>
         <div class="page"><span>Notifications</span></div>
       </a>
     </li>
     <li>
-      <a href="#" onmouseover="highlightIcon(this, '../../assets/icons/highlightedCreate.png')" onmouseout="unHighlightIcon(this, '../../assets/icons/create.png')">
-        <div class="icon"><img src="../../assets/icons/create.png" /></div>
+      <a href="#" class="menuOption">
+        <div class="icon"><img id="menuIcon" src="../../assets/icons/create.png" data-original-icon="../../assets/icons/create.png" data-highlighted-icon="../../assets/icons/highlightedCreate.png" /></div>
         <div class="page"><span>Create</span></div>
       </a>
     </li>
     <li>
-      <a href="<?php echo $myProfilePath; ?>" onmouseover="highlightIcon(this, '../../assets/icons/blackHome.png')" onmouseout="unHighlightIcon(this, '../../assets/icons/whiteHome.png')">
-        <div class="icon"><img src="../../assets/icons/home.png" /></div>
+      <a href="<?php echo $myProfilePath; ?>" class="menuOption">
+        <div class="icon"><img id="menuIcon" src="../../assets/icons/home.png" data-original-icon="../../assets/icons/home.png" data-highlighted-icon="../../assets/icons/blackHome.png" /></div>
         <div class="page"><span>Profile</span></div>
       </a>
     </li>
-    <li class="last-item">
-      <a href="<?php echo $logoutPath; ?>" onmouseover="highlightIcon(this, '../../assets/icons/blackHome.png')" onmouseout="unHighlightIcon(this, '../../assets/icons/whiteHome.png')">
-        <div class="icon"><img src="../../assets/icons/home.png" /></div>
+    <li class="last-item" id="menuOption">
+      <a href="<?php echo $logoutPath; ?>" class="menuOption">
+        <div class="icon"><img id="menuIcon" src="../../assets/icons/home.png" data-original-icon="../../assets/icons/home.png" data-highlighted-icon="../../assets/icons/blackHome.png" /></div>
         <div class="page"><span>Logout</span></div>
       </a>
     </li>
