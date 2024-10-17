4858dd050b398ce58096f326203eeaa5ea9169a9
gazi04 <gazmendhalili2016@gmail.com>
2024-10-17 22:05:48 +0200

    Adding a post handler for searching  users that is used in the search bar.

--- src/components/getUsers.php
+++ src/components/getUsers.php
@@ -1,13 +1,18 @@
 <?php
 require "../vendor/autoload.php";
 use Controllers\FollowController;
+use Controllers\UserController;
 
 $followController = new FollowController();
+$userController = new UserController();
 if(isset($_POST["getUsersThatIFollow"]) && isset($_POST["userId"])){
   echo $followController->getFollowings($_POST["userId"]);
 }
 else if(isset($_POST["getUsersThatFollowMe"]) && isset($_POST["userId"])){
   echo $followController->getFollowers($_POST["userId"]);
+}
+else if(isset($_POST["searchUser"]) && isset($_POST["term"])){
+  echo $userController->searchUsers($_POST["term"]);
 }
 exit();
 ?>
