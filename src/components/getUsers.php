25917def13e2f058a6d1931a7174a7c7f89e765c
gazi04 <gazmendhalili2016@gmail.com>
2024-10-18 11:05:33 +0200

    The searchUser method now also requires the user id from the logged user.

--- src/components/getUsers.php
+++ src/components/getUsers.php
@@ -12,7 +12,8 @@ else if(isset($_POST["getUsersThatFollowMe"]) && isset($_POST["userId"])){
   echo $followController->getFollowers($_POST["userId"]);
 }
 else if(isset($_POST["searchUser"]) && isset($_POST["term"])){
-  echo $userController->searchUsers($_POST["term"]);
+  session_start();
+  echo $userController->searchUsers($_POST["term"], $_SESSION["userId"]);
 }
 exit();
 ?>
