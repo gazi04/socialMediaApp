9f811c7122688ca95b4e1a4eed7f9156bf3e2c83
gazi04 <gazmendhalili2016@gmail.com>
2024-10-22 18:37:00 +0200

    Removing unnecessary controllers from the code.

--- src/views/feed/index.php
+++ src/views/feed/index.php
@@ -3,19 +3,11 @@ require_once "../../core/config.php";
 require_once "../../vendor/autoload.php";
 include \BASE_PATH . "/views/auth/check.php";
 
-use Controllers\UserController;
-use Controllers\PostController;
-use Controllers\FollowController;
 use Controllers\LikeController;
-use Controllers\CommentController;
 use Controllers\FeedController;
 
-$userController = new UserController();
 $feedController = new FeedController();
-$postController = new PostController();
-$followController = new FollowController();
 $likeController = new LikeController();
-$commentController = new CommentController();
 
 $profileUserId = $_SESSION["userId"];
 ?>
