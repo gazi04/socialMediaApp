e953dbd93f0688bd34197af7e5acad28cd06f112
gazi04 <gazmendhalili2016@gmail.com>
2024-10-27 18:10:18 +0200

    Now only getChatRooms method is responsible for getting the chat rooms from the database.

--- src/models/Message.php
+++ src/models/Message.php
@@ -28,34 +28,18 @@ class Message{
   }
 
   public function getChatRooms($userId){
-    $this->db->query("SELECT u.UserID, u.Username, u.ProfileImage, m.Message, m.CreateAt as MessageCreateAt
-      FROM messages AS m
-      INNER JOIN users AS u ON m.SenderID = u.UserID
-      INNER JOIN (
-          SELECT SenderID, MAX(CreateAt) AS LatestMessageTime
-          FROM messages
-          WHERE ReceiverID = :userId
-          GROUP BY SenderID
-      ) AS latest_messages ON m.SenderID = latest_messages.SenderID AND m.CreateAt = latest_messages.LatestMessageTime
-      WHERE m.ReceiverID = :userId
-      ORDER BY m.CreateAt DESC; 
+    $this->db->query(" SELECT u.UserID, u.Username, u.ProfileImage, m.Message, m.Seen, m.CreateAt as MessageCreateAt
+        FROM messages AS m
+        INNER JOIN users AS u ON m.SenderID = u.UserID
+        INNER JOIN (
+            SELECT SenderID, MAX(CreateAt) AS LatestMessageTime
+            FROM messages
+            WHERE ReceiverID = :userId
+            GROUP BY SenderID
+        ) AS latest_messages ON m.SenderID = latest_messages.SenderID AND m.CreateAt = latest_messages.LatestMessageTime
+        WHERE m.ReceiverID = :userId
+        ORDER BY m.Seen, m.CreateAt DESC; 
       ");
-    $this->db->bind(":userId", $userId);
-    return $this->db->execute();
-  }
-
-  public function recentMessages($userId){
-    $this->db->query("SELECT u.UserID, u.Username, u.ProfileImage, m.Message, m.CreateAt as MessageCreatAt
-      FROM messages AS m
-      INNER JOIN users AS u ON m.SenderID = u.UserID
-      INNER JOIN (
-        SELECT SenderID, MAX(CreateAt) AS LatestMessageTime
-        FROM messages
-        WHERE ReceiverID = :userId
-        GROUP BY SenderID
-      ) AS latest_messages ON m.SenderID = latest_messages.SenderID AND m.CreateAt = latest_messages.LatestMessageTime
-      WHERE m.ReceiverID = :userId and m.Seen = 0
-      ORDER BY m.CreateAt DESC; ");
     $this->db->bind(":userId", $userId);
     return $this->db->execute();
   }
