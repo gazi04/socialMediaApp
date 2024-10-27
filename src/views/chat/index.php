75c3e832052ff27759a74c44633430019be8a5b4
gazi04 <gazmendhalili2016@gmail.com>
2024-10-27 10:59:22 +0200

    Adding also a searching bar for search users to chat with.

--- src/views/chat/index.php
+++ src/views/chat/index.php
@@ -19,6 +19,11 @@ include \BASE_PATH . "/views/auth/check.php";
       <div id="chat-navbar"><?php include(BASE_PATH."/components/navbar.php"); ?></div>
 
       <div id="chat-rooms" class="usersList">
+        <div id="searchBar" style="padding-right: 1em;">
+          <svg class="icon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+          <input placeholder="Search" type="search" id="searchingTerm" class="input"/>
+        </div>
+
         <div class="user">
           <img src="../../assets/images/sunflower.jpg"/>
           <span class='username'>Test</span>
@@ -80,9 +85,9 @@ include \BASE_PATH . "/views/auth/check.php";
           <span class='username'>Test</span>
         </div>
         <div class="user">
-          <img src="../../assets/images/sunflower.jpg"/>
-          <span class='username'>Test</span>
-        </div>
+        <img src="../../assets/images/sunflower.jpg"/>
+        <span class='username'>Test</span>
+      </div>
         <div class="user">
           <img src="../../assets/images/sunflower.jpg"/>
           <span class='username'>Test</span>
@@ -107,13 +112,33 @@ include \BASE_PATH . "/views/auth/check.php";
 
       <div id="chat-room">
         <div id="messages">
+          <div class="received">
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+          </div>
           <div class="sended">
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
             <div class="message">hallo</div>
             <div class="message">hallo</div>
             <div class="message">hallo</div>
             <div class="message">hallo</div>
           </div>
           <div class="received">
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
+            <div class="message">hallo</div>
             <div class="message">hallo</div>
             <div class="message">hallo</div>
             <div class="message">hallo</div>
