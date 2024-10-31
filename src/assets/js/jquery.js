b11f5c5d2036af3f3580338fcd790e996d147836
gazi04 <gazmendhalili2016@gmail.com>
2024-10-31 10:34:14 +0200

    Removed the code that was used for navbar, because the code lives in the navbar.js file.

--- src/assets/js/jquery.js
+++ src/assets/js/jquery.js
@@ -115,18 +115,6 @@ $(document).ready(function() {
     window.location.href = "../profile/index.php?"+$.param(data);
   }
 
-  // HIGHLIGHT ICONS IF MOUSE IS OVER THE NAVBAR BUTTONS
-  $(".menuOption").hover(
-    function(){
-      const icon = $(this).children(".icon").children("img");
-      $(icon).prop("src", icon.data("highlighted-icon"));
-    },
-    function(){
-      const icon = $(this).children(".icon").children("img");
-      $(icon).prop("src", icon.data("original-icon"));
-    }
-  );
-
   // HANDLES LIKES IN THE FEED VIEW
   $(".likeButton").on("click", function(){
     const postid = $(this).data("postid");
@@ -310,35 +298,6 @@ $(document).ready(function() {
 
     const post = postsArray[currentIndex];
     fetchDataIntoPostModal(post);
-  });
-
-  // ADDS ANIMATION TO THE SEARCH BAR IF OPEN IT
-  $("#searchOption").on("click", function(e){
-    e.preventDefault();
-    $("#searchContainer").show().removeClass("fadeOutLeft");
-    $("#searchContainer").show().addClass("fadeInLeft");
-  });
-
-  $("#closeSearch").on("click", function(e){
-    e.preventDefault();
-    $("#searchContainer").show().removeClass("fadeInLeft");
-    $("#searchContainer").show().addClass("fadeOutLeft");
-  });
-
-  $("#searchingTerm").on("input", function(){
-    $.post("../../components/getUsers.php",
-      {
-        searchUser: true,
-        term: $(this).val()
-      },
-      function(response){ $("#users-list").html(response); }
-    );
-  });
-
-  // NAVBAR OPTION FOR THE LOGGED USER TO LOOK AT HIS PROFILE
-  $("#userProfile").on("click", function(){
-    const data = {"loggedUser": true};
-    window.location.href = "../profile/index.php?"+$.param(data);
   });
 
   $("#followOrUnFollowUser").on("click", function(){
