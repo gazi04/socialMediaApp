1e9b2fc137eb21b1115ce7b7c6014a15ca4ee837
gazi04 <gazmendhalili2016@gmail.com>
2024-11-01 21:02:10 +0200

    Added the php code to handle the request for the openRoom post request from chat.js.

--- src/components/chatHandler.php
+++ src/components/chatHandler.php
@@ -1,7 +1,9 @@
 <?php
 require "../vendor/autoload.php";
 use Controllers\MessageController;
+use Controllers\UserController;
 
+$userController = new UserController();
 $messageController = new MessageController();
 
 if(isset($_POST["getRooms"])){
@@ -9,5 +11,12 @@ if(isset($_POST["getRooms"])){
 }
 else if(isset($_POST["searchRooms"]) && isset($_POST["term"]) && !empty($_POST["term"])){
   $messageController->searchRooms($_POST["term"]);
+}
+else if(isset($_POST["openRoom"]) && isset($_POST["username"])){
+  session_start();
+  $user = $userController->getUserByName($_POST["username"]);
+  /* echo $_SESSION["userId"]."  "; */
+  /* echo $user["UserID"]; */
+  echo $messageController->getChatHistory($_SESSION["userId"], $user["UserID"]);
 }
 ?>
