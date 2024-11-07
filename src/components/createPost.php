b8385aaa3a7793af26f8a2b7203b9e1a5f2abae4
gazi04 <gazmendhalili2016@gmail.com>
2024-11-07 20:32:49 +0200

    Adding the pop up form for creating new posts and also adding css.

---
+++ src/components/createPost.php
@@ -0,0 +1,21 @@
+<dialog data-model id="createPost" style="width: 50%; height: 70vh;">
+  <div id="uploadNewPost"> 
+    <!-- <h1>Hello</h1> -->
+    <!-- <img src="../../assests/images/sunflower.jpg"/> -->
+    <!---->
+    <!-- <input type="file" id="imageInput" accept="image/*" style="display: none;"/> -->
+    <!-- <div><a id="uploadImage">Upload Image</a></div> -->
+    <!-- <button id="submitProfile">Submit</button> -->
+
+    <div class="image-preview" id="imagePreview">
+      <img src="../../assets/images/sunflower.jpg"/>
+    </div>
+
+    <div class="form-section">
+      <input type="text" id="caption" placeholder="Caption">
+      <a id="uploadImage"><img src="../../assets/icons/upload.png" /></a>
+      <a id="submit-button">Submit</a>
+      <input type="file" id="imageInput" accept="image/*" style="display: none;">
+    </div>
+  </div>
+</dialog>
