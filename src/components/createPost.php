1fadd4d18422def887e76921b47182504a24c3b6
gazi04 <gazmendhalili2016@gmail.com>
2024-11-08 11:08:03 +0200

    Removed the blurry effect for the image that is going to be uploaded and displayed in the page like a preview, and update the html code for the create post pop up.

--- src/components/createPost.php
+++ src/components/createPost.php
@@ -1,19 +1,12 @@
 <dialog data-model id="createPost" style="width: 50%; height: 70vh;">
   <div id="uploadNewPost"> 
-    <!-- <h1>Hello</h1> -->
-    <!-- <img src="../../assests/images/sunflower.jpg"/> -->
-    <!---->
-    <!-- <input type="file" id="imageInput" accept="image/*" style="display: none;"/> -->
-    <!-- <div><a id="uploadImage">Upload Image</a></div> -->
-    <!-- <button id="submitProfile">Submit</button> -->
-
-    <div class="image-preview" id="imagePreview">
-      <img src="../../assets/images/sunflower.jpg"/>
+    <div class="image-preview" id="uploadImage">
+      <img class="preview"/>
+      <div class="upload-icon"><img src="../../assets/icons/upload.png" /></div>
     </div>
 
     <div class="form-section">
-      <input type="text" id="caption" placeholder="Caption">
-      <a id="uploadImage"><img src="../../assets/icons/upload.png" /></a>
+      <input type="text" id="caption" placeholder="Add a Caption">
       <a id="submit-button">Submit</a>
       <input type="file" id="imageInput" accept="image/*" style="display: none;">
     </div>
